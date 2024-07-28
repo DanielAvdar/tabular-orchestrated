0dcbf890c998d8e97bd3138040f17cb372e14643
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-07-28 13:16:17 +0300

    Refactor and restructure test suite
    Reorganized the test structure by splitting large test files and refactoring test fixtures for better modularity and maintainability. The tests are now categorized into separate directories and files for MLJAR and DeepChecks related tests, enhancing readability and ease of navigation.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

---
+++ tests/comp/mljar/conftest.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from tabular_orchestrated.components import DataSplitter
+from tabular_orchestrated.deepchecks import DCModelComp
+from tabular_orchestrated.mljar import EvaluateMLJAR, MLJARTraining
+
+import pytest
+from ml_orchestrator import artifacts
+
+
+@pytest.fixture(scope="session")
+def mljar_training_op(get_df_example: artifacts.Dataset, split_op: DataSplitter) -> MLJARTraining:
+    tmp_files_folder = Path(get_df_example.uri).parent
+
+    def func(x):
+        return (tmp_files_folder / x).as_posix()
+
+    mljar_training_op = MLJARTraining(
+        dataset=split_op.train_dataset,
+        model=artifacts.Model(uri=func("model")),
+    )
+    mljar_training_op.execute()
+    return mljar_training_op
+
+
+@pytest.fixture(scope="session")
+def eval_mljar_op(
+    get_df_example: artifacts.Dataset, split_op: DataSplitter, mljar_training_op: MLJARTraining
+) -> EvaluateMLJAR:
+    tmp_files_folder = Path(get_df_example.uri).parent
+
+    def func(x):
+        return (tmp_files_folder / x).as_posix()
+
+    eval_mljar_op = EvaluateMLJAR(
+        test_dataset=split_op.test_dataset,
+        model=mljar_training_op.model,
+        metrics=artifacts.Metrics(uri=func("metrics")),
+        report=artifacts.HTML(uri=func("report.html")),
+    )
+    eval_mljar_op.execute()
+    return eval_mljar_op
+
+
+@pytest.fixture(scope="session")
+def deepchecks_model_op(
+    get_df_example: artifacts.Dataset, split_op: DataSplitter, mljar_training_op: MLJARTraining
+) -> DCModelComp:
+    tmp_files_folder = Path(get_df_example.uri).parent
+
+    def func(x):
+        return (tmp_files_folder / x).as_posix()
+
+    deepchecks_model_op = DCModelComp(
+        train_dataset=split_op.train_dataset,
+        test_dataset=split_op.test_dataset,
+        model=mljar_training_op.model,
+        report=artifacts.HTML(uri=func("deepchecks_model.html")),
+        failed_checks=artifacts.Metrics(uri=func("failed_checks_model")),
+    )
+    deepchecks_model_op.execute()
+    return deepchecks_model_op
