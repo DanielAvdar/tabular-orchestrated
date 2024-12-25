0a75a3b1a6d10b0358fbf676f399f5af784c7a04
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-12-25 18:15:59 +0200

    Remove unused MljarDeepChecks and related test dependencies
    Deleted the `mljar_deepchecks.py` file and cleaned up unused dataset imports in the `test_mljar.py` file. These components were unnecessary and no longer aligned with the project's requirements. This simplifies the codebase and removes redundant dependencies.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/comp/mljar/test_mljar.py
+++ tests/comp/mljar/test_mljar.py
@@ -1,20 +1,8 @@
 from pathlib import Path
 
 from ml_orchestrator import artifacts
-from sklearn import datasets
 
 from tabular_orchestrated.mljar.mljar import EvaluateMLJAR, MLJARTraining
-
-dataset_importers = [
-    # (datasets.load_iris,'iris'),
-    # (datasets.load_wine,'wine'),
-    # (datasets.load_diabetes,'diabetes'),
-    # (datasets.load_breast_cancer,'breast_cancer'),
-    datasets.load_diabetes,
-    datasets.load_breast_cancer,
-    datasets.load_iris,
-    datasets.load_wine,
-]
 
 
 def test_train_mljar(get_df_example: artifacts.Dataset, mljar_training_op: MLJARTraining) -> None:
