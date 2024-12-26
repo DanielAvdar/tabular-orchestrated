b6a0ec3403c12a91010c0ee8ffad03f8cecd1cb8
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-12-26 17:42:29 +0200

    Update import to use DCModelCompV2 in deepchecks test
    Replaced DCModelComp with its updated version, DCModelCompV2, in the test function for deepchecks imports. This ensures the test aligns with the latest component naming conventions.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/imports_checks/test_deepchecks_imports.py
+++ tests/imports_checks/test_deepchecks_imports.py
@@ -1,4 +1,4 @@
 def test_dc_imports():
-    from tabular_orchestrated.dc import DCTrainTestComp, DCDataComp, DCFullComp, DCModelComp  # noqa
+    from tabular_orchestrated.dc import DCTrainTestComp, DCDataComp, DCModelCompV2  # noqa
 
     assert True
